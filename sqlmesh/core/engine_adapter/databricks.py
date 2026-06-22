444c50d77a67f785cd532d1011804df29989f10a
davem-bis <68955845+davem-bis@users.noreply.github.com>
2026-06-22 16:13:18 +0100

    fix: Databricks column type identification (#5848)
    Signed-off-by: davem-bis <68955845+davem-bis@users.noreply.github.com>

--- sqlmesh/core/engine_adapter/databricks.py
+++ sqlmesh/core/engine_adapter/databricks.py
@@ -431,7 +431,7 @@ class DatabricksEngineAdapter(SparkEngineAdapter, GrantsFromInfoSchemaMixin):
             .order_by("ordinal_position ASC")
         )
 
-        self.cursor.execute(query)
+        self.execute(query.sql(dialect=self.dialect))
         result = self.cursor.fetchall()
 
         return {row[0]: exp.DataType.build(row[1], dialect=self.dialect) for row in result}
